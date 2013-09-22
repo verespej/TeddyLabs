7751586f9a6bcf7cadbfb6f65c8d307b1aab9afe
Grant Timmerman <granttimmerman@gmail.com>
2013-09-21 21:31:04 -0700

    bug fiz

--- public/js/home.js
+++ public/js/home.js
@@ -8,9 +8,9 @@ $(function () {
 			alert('Please enter a valid email address');
 		} else {
 			// Post the email
-			// $.post('/email/', $(this).serialize()).done(function () {
-			// 	console.log('successful email post');
-			// });
+			$.post('/email/', $(this).serialize()).done(function () {
+				console.log('successful email post');
+			});
 		}
 
 		$('.card').addClass('flipped');
