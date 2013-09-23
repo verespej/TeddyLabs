0d9e8f5d5f08fd59c097cf8d98c9e0d2e6c4ed83
Grant Timmerman <granttimmerman@gmail.com>
2013-09-22 17:03:24 -0700

    Using cached version

--- public/prototype/selectionflow.js
+++ public/prototype/selectionflow.js
@@ -135,15 +135,15 @@ $(document).ready(function(){
     // Start the game
     */
 
-  // var dataFile = 'apicall.json';
+  var dataFile = 'apicall.json';
   // var dataFile = "/api/toys?" + window.document.URL.toString().split("?")[1];
-  var dataFile = "";
+  // var dataFile = "";
 
-  if (getURLParameters("cached")) {
-    dataFile = 'apicall.json';
-  } else {
-    dataFile = "/api/toys?" + window.document.URL.toString().split("?")[1];
-  }
+  // if (getURLParameters("cached")) {
+  //   dataFile = 'apicall.json';
+  // } else {
+  //   dataFile = "/api/toys?" + window.document.URL.toString().split("?")[1];
+  // }
 
   $.getJSON(dataFile, function(data){
     var results = data.results;
